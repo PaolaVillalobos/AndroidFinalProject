03e7c0dfc2d64cc693f44eb622884543dabac6ca
davidag99 <davidarevalograu@gmail.com>
2023-04-29 20:29:31 +0200

    Logic of authentication done.

--- app/src/main/java/com/exercise/androidfinalproject/LoginActivity.java
+++ app/src/main/java/com/exercise/androidfinalproject/LoginActivity.java
@@ -1,59 +1,76 @@
 package com.exercise.androidfinalproject;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
 
-        EditText email = findViewById(R.id.etEmail);
-        EditText password = findViewById(R.id.etPassword);
-        TextView login = findViewById(R.id.tvLogin);
-        TextView forgotPassword = findViewById(R.id.tvForgotPassword);
-        TextView feedback = findViewById(R.id.tvFeedback);
+        EditText lEmail = findViewById(R.id.etEmail);
+        EditText lPassword = findViewById(R.id.etPassword);
+        TextView lLogin = findViewById(R.id.tvLogin);
+        TextView lFeedback = findViewById(R.id.tvFeedback);
+        TextView lSignup = findViewById(R.id.tvSignup);
 
-        SharedPreferences sharedPreferences = getSharedPreferences("APP_PREFERENCES",MODE_PRIVATE);
+        auth = FirebaseAuth.getInstance();
 
-        login.setOnClickListener(new View.OnClickListener() {
+        lLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String inputText = email.getText().toString().trim();
+                String inputText = lEmail.getText().toString().trim();
                 if(isEmailValid(inputText)){
-                    if(password.getText().toString().length() >= 6) {
-                        if(password.getText().toString().equals(sharedPreferences.getString("password"+email.getText().toString(),"123456"))){
-                            Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
-                            intent.putExtra("key_email", email.getText().toString());
-                            startActivity(intent);
-                            finish();
-                        }else{
-                            feedback.setText("Invalid Password. Try Again");
-                            feedback.setTextColor(Color.RED);
-                        }
-                    }else{
-                        feedback.setText("Invalid Password. Try Again");
-                        feedback.setTextColor(Color.RED);
-                    }
+                    if(lPassword.getText().toString().length() >= 6) {
 
-                }else{
-                    feedback.setText("Invalid Password. Try Again");
-                    feedback.setTextColor(Color.RED);
+                        String emailUser = lEmail.getText().toString().trim();
+                        String passwordUser = lPassword.getText().toString().trim();
+
+                        signIn( emailUser, passwordUser);
+
+                    }else {
+                        lFeedback.setText("Invalid Password. Try Again");
+                        lFeedback.setTextColor(Color.RED);
+                    }
                 }
             }
         });
+
+
+        lSignup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+        });
+
     }
 
     public static boolean isEmailValid(String email) {
@@ -62,4 +79,29 @@ public class LoginActivity extends AppCompatActivity {
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
+
+    private void signIn(String email, String password) {
+
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            //FirebaseUser user = mAuth.getCurrentUser();
+
+                            Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+                            //intent.putExtra("key_email", etEmail.getText().toString());
+                            startActivity(intent);
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        }
+                    }
+                });
+    }
+
 }
---
+++ app/src/main/java/com/exercise/androidfinalproject/ProfileActivity.java
@@ -0,0 +1,54 @@
+package com.exercise.androidfinalproject;
+
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_layout);
+
+        TextView lName = findViewById(R.id.etName);
+        Button lChangeData = findViewById(R.id.btChangeData);
+        Button lLogOut = findViewById(R.id.btLogOut);
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+
+            lName.setText(name);
+
+        }
+    }
+}
